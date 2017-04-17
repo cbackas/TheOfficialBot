7b2b2bcdc491dc6a46afeb09b437b03166b82af0
cbackas <cbackasSE@gmail.com>
2017-04-17 14:58:11 -0500

    Add ?user command

---
+++ src/main/java/cback/commands/CommandUserInfo.java
@@ -0,0 +1,78 @@
+package cback.commands;
+
+import cback.OfficialRoles;
+import cback.TheOfficialBot;
+import cback.Util;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IRole;
+import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.util.EmbedBuilder;
+
+import java.awt.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CommandUserInfo implements Command {
+    @Override
+    public String getName() {
+        return "userinfo";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Arrays.asList("user");
+    }
+
+    @Override
+    public String getSyntax() {
+        return "?user @user";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Returns information about a discord user";
+    }
+
+    @Override
+    public List<String> getPermissions() {
+        return Arrays.asList(OfficialRoles.STAFF.id);
+    }
+
+    @Override
+    public void execute(TheOfficialBot bot, IDiscordClient client, String[] args, IGuild guild, IMessage message, boolean isPrivate) {
+        if (message.getAuthor().getRolesForGuild(guild).contains(guild.getRoleByID(OfficialRoles.STAFF.id))) {
+
+            String text = message.getContent();
+            Pattern pattern = Pattern.compile("^\\?user <@!?(\\d+)>");
+            Matcher matcher = pattern.matcher(text);
+            if (matcher.find()) {
+                IUser user = guild.getUserByID(matcher.group(1));
+                String isBot = user.isBot() ? "yes":"no";
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm:ss");
+                List<IRole> roles = user.getRolesForGuild(guild);
+
+                EmbedBuilder embed = new EmbedBuilder();
+
+                embed
+                        .withAuthorIcon(user.getAvatarURL())
+                        .withAuthorName(user.getName())
+                        .withDesc("\uD83C\uDD94: ``" + user.getID() + "``"
+                        + "\n\uD83E\uDD16 **Bot**: ``" + isBot + "``"
+                        + "\n\uD83D\uDCE5 **Joined Server**: ``" + guild.getJoinTimeForUser(user).format(formatter) + "``"
+                        + "\n\uD83C\uDF10 **Joined Discord**: ``" + user.getCreationDate().format(formatter) + "``"
+                        + "\n\u2139 **Status**: ``" + user.getPresence().toString() + "``"
+                        + "\n\uD83D\uDEE1 **Roles**: ``" + roles.size() + " - " + roles.toString() + "``")
+                        .withFooterIcon(message.getAuthor().getAvatarURL())
+                        .withFooterText("Requested by: " + Util.getTag(message.getAuthor()))
+                        .withColor(Color.gray);
+
+                Util.sendEmbed(message.getChannel(), embed.build());
+            }
+        }
+    }
+}
