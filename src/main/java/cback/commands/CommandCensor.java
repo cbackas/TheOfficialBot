f087b9e1996ac9083a0cc1556c7ba4ea99b1ab92
cbackas <cbackasSE@gmail.com>
2017-04-25 15:51:25 -0500

    add "list" to the command syntax

--- src/main/java/cback/commands/CommandCensor.java
+++ src/main/java/cback/commands/CommandCensor.java
@@ -25,7 +25,7 @@ public class CommandCensor implements Command {
 
     @Override
     public String getSyntax() {
-        return "?censor add|remove [word]";
+        return "?censor add|remove|list [word]";
     }
 
     @Override
