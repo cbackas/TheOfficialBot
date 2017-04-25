c0fa2c769c0574f47db30e15074b0f82e0155841
cbackas <cbackasSE@gmail.com>
2017-04-25 15:34:05 -0500

    change to be stored in "long" format

--- src/main/java/cback/OfficialRoles.java
+++ src/main/java/cback/OfficialRoles.java
@@ -1,15 +1,15 @@
 package cback;
 
 public enum OfficialRoles {
-    STAFF("STAFF", "266651534635433985"),
-    ADMIN("ADMIN", "266650410373218305"),
-    HOST("HOST", "279962204135227393"),
-    MOD("MOD", "266651027665584128");
+    STAFF("STAFF", 266651534635433985L),
+    ADMIN("ADMIN", 266650410373218305L),
+    HOST("HOST", 279962204135227393L),
+    MOD("MOD", 266651027665584128L);
 
     public String name;
-    public String id;
+    public Long id;
 
-    OfficialRoles(String name, String id) {
+    OfficialRoles(String name, Long id) {
         this.name = name;
         this.id = id;
     }
