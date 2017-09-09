7f7aeabf1532f10d0ecc29b40c8c6f0f4b08eda2
cbackas <cbackasSE@gmail.com>
2017-09-08 23:51:17 -0500

    Added a pardoned message for message logs

--- src/main/java/cback/events/MemberChange.java
+++ src/main/java/cback/events/MemberChange.java
@@ -7,6 +7,7 @@ import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.member.UserBanEvent;
 import sx.blah.discord.handle.impl.events.guild.member.UserJoinEvent;
 import sx.blah.discord.handle.impl.events.guild.member.UserLeaveEvent;
+import sx.blah.discord.handle.impl.events.guild.member.UserPardonEvent;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.EmbedBuilder;
@@ -17,6 +18,11 @@ import java.util.List;
 
 public class MemberChange {
     private TheOfficialBot bot;
+
+    private final long MUTED_ROLE_ID = 269638591112544267l;
+    private final long MEMBERLOG_CH_ID = 266655441449254914l;
+    private final long STAFF_CH_ID = 266651712826114048l;
+    private final long ADMIN_CH_ID = 285470408709373954l;
 
     public MemberChange(TheOfficialBot bot) {
         this.bot = bot;
@@ -30,7 +36,7 @@ public class MemberChange {
             //Mute Check
             if (bot.getConfigManager().getConfigArray("muted").contains(user.getStringID())) {
                 try {
-                    user.addRole(guild.getRoleByID(Long.parseLong("269638591112544267")));
+                    user.addRole(guild.getRoleByID(MUTED_ROLE_ID));
                 } catch (Exception e) {
                     Util.reportHome(e);
                 }
@@ -43,7 +49,7 @@ public class MemberChange {
             //Notifier
             int totalUsers = guild.getUsers().size();
             if (totalUsers % 1000 == 0) {
-                Util.sendMessage(guild.getChannelByID(285470408709373954l), guild.getRoleByID(OfficialRoles.ADMIN.id).mention() + " we have hit " + totalUsers + " users hype");
+                Util.sendMessage(guild.getChannelByID(ADMIN_CH_ID), guild.getRoleByID(OfficialRoles.ADMIN.id).mention() + " we have hit " + totalUsers + " users hype");
             }
 
             //Member-log
@@ -58,9 +64,9 @@ public class MemberChange {
 
             bld
                     .withTimestamp(System.currentTimeMillis())
-                    .withColor(Color.GREEN);
+                    .withColor(Color.CYAN);
 
-            Util.sendEmbed(guild.getChannelByID(266655441449254914l), bld.build());
+            Util.sendEmbed(guild.getChannelByID(MEMBERLOG_CH_ID), bld.build());
         }
     }
 
@@ -72,7 +78,7 @@ public class MemberChange {
 
             //Mute Check
             if (bot.getConfigManager().getConfigArray("muted").contains(event.getUser().getStringID())) {
-                Util.sendMessage(guild.getChannelByID(Long.parseLong("266651712826114048")), user + " is muted and left the server. Their mute will be applied again when/if they return.");
+                Util.sendMessage(guild.getChannelByID(STAFF_CH_ID), user + " is muted and left the server. Their mute will be applied again when/if they return.");
             }
 
             //Leave Counter
@@ -85,7 +91,7 @@ public class MemberChange {
                     .withTimestamp(System.currentTimeMillis())
                     .withColor(Color.YELLOW);
 
-            Util.sendEmbed(guild.getChannelByID(266655441449254914l), bld.build());
+            Util.sendEmbed(guild.getChannelByID(MEMBERLOG_CH_ID), bld.build());
         }
     }
 
@@ -112,7 +118,23 @@ public class MemberChange {
                     .withTimestamp(System.currentTimeMillis())
                     .withColor(Color.RED);
 
-            Util.sendEmbed(guild.getChannelByID(266655441449254914l), bld.build());
+            Util.sendEmbed(guild.getChannelByID(MEMBERLOG_CH_ID), bld.build());
+        }
+    }
+
+    @EventSubscriber
+    public void memberUnbanned(UserPardonEvent event) {
+        if (event.getGuild().getStringID().equals(TheOfficialBot.getHomeGuild().getStringID())) {
+            IUser user = event.getUser();
+            IGuild guild = event.getGuild();
+
+            //Member-log
+            EmbedBuilder bld = new EmbedBuilder()
+                    .withDesc(Util.getTag(user) + " was **unbanned** from the server. " + user.mention())
+                    .withTimestamp(System.currentTimeMillis())
+                    .withColor(Color.GREEN);
+
+            Util.sendEmbed(guild.getChannelByID(MEMBERLOG_CH_ID), bld.build());
         }
     }
 }
