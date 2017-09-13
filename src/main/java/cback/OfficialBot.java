a42932becc6783d2b54a1cd7066e1e1498fab11e
cbackas <cbackasSE@gmail.com>
2017-09-13 15:01:43 -0500

    Made messageCache public so that instances aren't happening

--- src/main/java/cback/OfficialBot.java
+++ src/main/java/cback/OfficialBot.java
@@ -39,7 +39,7 @@ public class OfficialBot {
 
     public static List<Command> registeredCommands = new ArrayList<>();
 
-    private ArrayList<Long> messageCache = new ArrayList<>();
+    public static ArrayList<Long> messageCache = new ArrayList<>();
 
     static private String prefix = "?";
     private static final Pattern COMMAND_PATTERN = Pattern.compile("^\\?([^\\s]+) ?(.*)", Pattern.CASE_INSENSITIVE);
@@ -208,10 +208,6 @@ public class OfficialBot {
 
     public static Color getBotColor() {
         return Color.decode("#" + configManager.getConfigValue("bot_color"));
-    }
-
-    public ArrayList<Long> getMessageCache() {
-        return messageCache;
     }
 
     private void registerAllCommands() {
