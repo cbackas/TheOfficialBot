fe0915dd7e9953963cb453d5960ac60152c41dea
cbackas <cbackasSE@gmail.com>
2018-01-26 15:45:58 -0600

    Kinda rewrote the mute command and fixed it

--- src/main/java/cback/commands/CommandMuteAdd.java
+++ src/main/java/cback/commands/CommandMuteAdd.java
@@ -5,11 +5,11 @@ import cback.OfficialBot;
 import cback.ServerLog;
 import cback.Util;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -41,69 +41,81 @@ public class CommandMuteAdd implements Command {
         return Arrays.asList(OfficialRoles.STAFF.id);
     }
 
+    OfficialBot bot = OfficialBot.getInstance();
+
     @Override
     public void execute(IMessage message, String content, String[] args, IUser author, IGuild guild, List<Long> roleIDs, boolean isPrivate, IDiscordClient client, OfficialBot bot) {
-        List<String> mutedUsers = bot.getConfigManager().getConfigArray("muted");
-
-        if (args[0].equalsIgnoreCase("list")) {
-
-            StringBuilder mutedList = new StringBuilder();
-            if (!mutedUsers.isEmpty()) {
-                for (String userID : mutedUsers) {
-
-                    IUser userO = guild.getUserByID(Long.parseLong(userID));
-
-                    String user = "<@" + userID + ">";
-                    if (userO != null) {
-                        user = userO.mention();
-                    }
-
-                    mutedList.append("\n").append(user);
-                }
+        if (args.length >= 1) {
+            if (args[0].equalsIgnoreCase("list")) {
+                muteList(message.getChannel());
+            } else if (args[0].matches("<@!?(\\d)+>")) {
+                muteUser(message);
             } else {
-                mutedList.append("\n").append("There are currently no muted users.");
+                Util.syntaxError(this, message);
             }
-
-            Util.simpleEmbed(message.getChannel(), "Muted Users: (plain text for users not on server)\n" + mutedList.toString());
-
-        } else if (args.length >= 1) {
-            Pattern pattern = Pattern.compile("^!mute <@!?(\\d+)> ?(.+)?");
-            Matcher matcher = pattern.matcher(content);
-
-            if (matcher.find()) {
-                String u = matcher.group(1);
-                String reason = matcher.group(2);
-
-                IUser userInput = guild.getUserByID(Long.parseLong(u));
-                if (userInput != null) {
-                    if (reason == null) {
-                        reason = "an unspecified reason";
-                    }
-
-                    if (message.getAuthor().getStringID().equals(u)) {
-                        Util.simpleEmbed(message.getChannel(), "You probably shouldn't mute yourself");
-                    } else {
-                        try {
-                            userInput.addRole(guild.getRoleByID(231269949635559424l));
-                            Util.simpleEmbed(message.getChannel(), userInput.getDisplayName(guild) + " has been muted. Check " + guild.getChannelByID(OfficialBot.SERVERLOG_CH_ID).mention() + " for more info.");
-
-                            if (!mutedUsers.contains(u)) {
-                                mutedUsers.add(u);
-                                bot.getConfigManager().setConfigValue("muted", mutedUsers);
-                            }
-
-                            new ServerLog(author, "Muted " + userInput.getDisplayName(guild) + "\n**Reason:** " + reason).send();
-                        } catch (Exception e) {
-                            Util.simpleEmbed(message.getChannel(), "Error running " + this.getName() + " - error recorded");
-                            Util.reportHome(message, e);
-                        }
-                    }
-                }
-            }
-        } else {
-            Util.syntaxError(this, message);
         }
         Util.deleteMessage(message);
     }
 
+    private void muteList(IChannel channel) {
+        List<String> mutedUsers = bot.getConfigManager().getConfigArray("muted");
+        StringBuilder mutedList = new StringBuilder();
+        if (!mutedUsers.isEmpty()) {
+            for (String userID : mutedUsers) {
+                IUser userO = OfficialBot.getClient().getUserByID(Long.parseLong(userID));
+
+                String user = "<@" + userID + ">";
+                if (userO != null) {
+                    user = userO.mention();
+                }
+
+                mutedList.append("\n").append(user);
+            }
+        } else {
+            mutedList.append("\n").append("There are currently no muted users.");
+        }
+
+        Util.simpleEmbed(channel, "Muted Users: (plain text for users not on server)\n" + mutedList.toString());
+    }
+
+    private void muteUser(IMessage message) {
+        IGuild guild = message.getGuild();
+        String stuff = message.getContent().split(" ", 2)[1];
+
+        Pattern pattern = Pattern.compile("^<@!?(\\d+)> ?(.+)?");
+        Matcher matcher = pattern.matcher(stuff);
+        if (matcher.find()) {
+            String u = matcher.group(1);
+            String reason = matcher.group(2);
+
+            IUser user = guild.getUserByID(Long.parseLong(u));
+            if (user != null) {
+                if (reason == null) {
+                    reason = "an unspecified reason";
+                }
+
+                if (message.getAuthor().getStringID().equals(u)) {
+                    Util.simpleEmbed(message.getChannel(), "You probably shouldn't mute yourself");
+                    return;
+                }
+            }
+
+            try {
+                List<String> mutedUsers = bot.getConfigManager().getConfigArray("muted");
+
+                user.addRole(guild.getRoleByID(Long.parseLong("281022564002824192")));
+                Util.simpleEmbed(message.getChannel(), user.getDisplayName(guild) + " has been muted. Check " + guild.getChannelByID(OfficialBot.SERVERLOG_CH_ID).mention() + " for more info.");
+
+                if (!mutedUsers.contains(user.getStringID())) {
+                    mutedUsers.add(user.getStringID());
+                    bot.getConfigManager().setConfigValue("muted", mutedUsers);
+                }
+
+                new ServerLog(message.getAuthor(), "Muted " + user.getDisplayName(guild) + "\n**Reason:** " + reason).send();
+            } catch (Exception e) {
+                Util.simpleEmbed(message.getChannel(), "Error running " + this.getName() + " - error recorded");
+                Util.reportHome(message, e);
+            }
+        }
+    }
 }
